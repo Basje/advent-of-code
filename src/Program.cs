7b3823a57635b0e62a01552a7fb9cbc50a8dc941
Bas Hamar de la Brethonière <Basje@users.noreply.github.com>
2019-12-03 15:53:57 +0100

    Add calculation and output for first half of the Day 1 puzzle

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using AdventOfCode_2019.Day_01;
 
 namespace AdventOfCode_2019
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Advent of Code 2019");
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            var fileLocation = @$"{Directory.GetCurrentDirectory()}\Day-01\input.txt";
+
+            if (!File.Exists(fileLocation))
+            {
+                Console.WriteLine($"Input file '{fileLocation}' could not be found. Aborting...");
+                return;
+            }
+
+            var inputs = File.ReadAllLines(fileLocation);
+            var sum = 0;
+
+            foreach (var input in inputs)
+            {
+                if (int.TryParse(input, out int mass))
+                {
+                    sum += FuelCounterUpper.CalculateForMass(mass);
+                }
+            }
+
+            Console.WriteLine($"Total fuel requirement: {sum}");
         }
     }
 }
