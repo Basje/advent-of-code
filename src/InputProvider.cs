3f1e3e1558db4ab7cfcc74feb866ef4c910e995e
Bas Hamar de la Brethonière <Basje@users.noreply.github.com>
2019-12-04 15:00:55 +0100

    Add file reader for input files

---
+++ src/InputProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode_2019
+{
+    internal static class InputProvider
+    {
+        internal static IEnumerable<int> GetIntegers(string relativePath)
+        {
+            return GetStrings(relativePath)
+                .Select(input => int.Parse(input));
+        }
+
+        internal static IEnumerable<string> GetStrings(string relativePath)
+        {
+            return ReadLines(BuildFullPath(relativePath));
+        }
+
+        private static string BuildFullPath(string relativePath)
+        {
+            relativePath = NaiveFilter(relativePath);
+
+            return @$"{Directory.GetCurrentDirectory()}\{relativePath}";
+        }
+
+        private static string NaiveFilter(string key)
+        {
+            return key.Trim('\\', ' ', '.', '/');
+        }
+
+        private static IEnumerable<string> ReadLines(string fullPath)
+        {
+            if (!File.Exists(fullPath))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(fullPath);
+        }
+    }
+}
