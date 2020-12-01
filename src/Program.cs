699d452f1f40de7c5f93f3f1bea74dbb3ef73a56
Basje <Basje@users.noreply.github.com>
2020-12-01 20:18:34 +0100

    Add .NET Core command line application project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode_2020
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
