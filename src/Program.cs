979a621598f444dc5c558d9d800731ba5a7eb126
Basje <Basje@users.noreply.github.com>
2020-12-01 21:19:04 +0100

    Add lazy version of Day 01 solution

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,42 @@ namespace AdventOfCode_2020
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputProvider = new InputProvider<int>(1, 1);
+            var inputs = inputProvider.GetInputs();
+
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = i + 1; j < inputs.Count; j++)
+                {
+                    var left = inputs[i];
+                    var right = inputs[j];
+                    var sum = left + right;
+
+                    if (sum == 2020)
+                    {
+                        Console.WriteLine($"Day 01 part 1: {left * right}");
+                    }
+                }
+            }
+
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = i + 1; j < inputs.Count; j++)
+                {
+                    for (int k = i + 2; k < inputs.Count; k++)
+                    {
+                        var left = inputs[i];
+                        var middle = inputs[j];
+                        var right = inputs[k];
+                        var sum = left + middle + right;
+
+                        if (sum == 2020)
+                        {
+                            Console.WriteLine($"Day 01 part 2: {left * middle * right}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
