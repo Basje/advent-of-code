61d3aa53fcfc1768a37e2857e59fa440b6e8ecc2
Basje <Basje@users.noreply.github.com>
2020-12-03 21:36:39 +0100

    Clean up Day 01 solution and add improved output

--- 2020/src/Day01/Solution.cs
+++ 2020/src/Day01/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,26 +14,14 @@ namespace AdventOfCode_2020.Day01
         {
         }
 
-        public override Task<int> OnExecuteAsync(CommandLineApplication app, CancellationToken cancellationToken)
+        /// <summary>
+        /// Searches for three values in the input list which have the specified sum.
+        /// </summary>
+        /// <param name="sum">The requested sum of the three values.</param>
+        /// <param name="inputs">The integers that we can use.</param>
+        /// <returns></returns>
+        public int[] FindSumTriple(int sum, IList<int> inputs)
         {
-            var inputProvider = new InputProvider<int>(1, 1);
-            var inputs = inputProvider.GetInputs();
-
-            for (int i = 0; i < inputs.Count; i++)
-            {
-                for (int j = i + 1; j < inputs.Count; j++)
-                {
-                    var left = inputs[i];
-                    var right = inputs[j];
-                    var sum = left + right;
-
-                    if (sum == 2020)
-                    {
-                        Console.WriteLine($"Day 01 part 1: {left * right}");
-                    }
-                }
-            }
-
             for (int i = 0; i < inputs.Count; i++)
             {
                 for (int j = i + 1; j < inputs.Count; j++)
@@ -42,16 +31,60 @@ namespace AdventOfCode_2020.Day01
                         var left = inputs[i];
                         var middle = inputs[j];
                         var right = inputs[k];
-                        var sum = left + middle + right;
 
-                        if (sum == 2020)
+                        if (sum == left + middle + right)
                         {
-                            Console.WriteLine($"Day 01 part 2: {left * middle * right}");
+                            return new int[] { left, middle, right };
                         }
                     }
                 }
             }
+            return Array.Empty<int>();
+        }
 
+        /// <summary>
+        /// Searches for two values in the input list which have the specified sum.
+        /// </summary>
+        /// <param name="sum">The requested sum of the two values</param>
+        /// <param name="inputs">The integers that we can use.</param>
+        /// <returns></returns>
+        public int[] FindSumTuple(int sum, IList<int> inputs)
+        {
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = i + 1; j < inputs.Count; j++)
+                {
+                    var left = inputs[i];
+                    var right = inputs[j];
+
+                    if (sum == left + right)
+                    {
+                        return new int[] { left, right };
+                    }
+                }
+            }
+            return Array.Empty<int>();
+        }
+
+        public override Task<int> OnExecuteAsync(CommandLineApplication app, CancellationToken cancellationToken)
+        {
+            const int YEAR = 2020;
+
+            var inputProvider = new InputProvider<int>(1, 1);
+            var inputs = inputProvider.GetInputs();
+
+            var firstResult = FindSumTuple(YEAR, inputs);
+            var secondResult = FindSumTriple(YEAR, inputs);
+
+            WriteHeaderLine("Day 1; part 1");
+            WriteVerboseLine($"{firstResult[0]} + {firstResult[1]} = {firstResult[0] + firstResult[1]}");
+            WriteSuccessLine($"{firstResult[0]} * {firstResult[1]} = {firstResult[0] * firstResult[1]}");
+
+            WriteLine();
+
+            WriteHeaderLine("Day 1; part 2");
+            WriteVerboseLine($"{secondResult[0]} + {secondResult[1]} * {secondResult[2]} = {secondResult[0] + secondResult[1] + secondResult[1]}");
+            WriteSuccessLine($"{secondResult[0]} * {secondResult[1]} * {secondResult[2]} = {secondResult[0] * secondResult[1] * secondResult[1]}");
             return Task.FromResult(0);
         }
     }
