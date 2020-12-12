0f945e8b2bec6891d38a95e12788743df01d0916
Basje <Basje@users.noreply.github.com>
2020-12-12 21:01:21 +0100

    Add Day 07 part 1 solution

--- 2020/src/Day07/ExtensionMethods.cs
+++ 2020/src/Day07/ExtensionMethods.cs
@@ -1,6 +1,67 @@
-﻿namespace AdventOfCode_2020.Day07
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode_2020.Day07
 {
     public static class ExtensionMethods
     {
+        public static Dictionary<int, IDictionary<int, int>> DetermineBagContentsWith(this IList<string> input, IDictionary<string, int> translationTable)
+        {
+            var possibleContentsPerBagType = new Dictionary<int, IDictionary<int, int>>();
+
+            input
+                .ToList()
+                .ForEach(rule => possibleContentsPerBagType.Add(possibleContentsPerBagType.Count, rule.GetBagContentsWith(translationTable)));
+
+            return possibleContentsPerBagType;
+        }
+
+        public static Dictionary<string, int> DetermineBagTypes(this IList<string> input)
+        {
+            var bagTypes = new Dictionary<string, int>();
+
+            input
+                .ToList()
+                .ForEach(rule => bagTypes.Add(rule.GetSubjectBagType(), bagTypes.Count));
+
+            return bagTypes;
+        }
+
+        public static IDictionary<int, int> GetBagContentsWith(this string rule, IDictionary<string, int> translationTable)
+        {
+            var possibleContents = new Dictionary<int, int>();
+
+            var contents = rule
+                .Split("contain ")
+                .Last()
+                .Replace(".", "")
+                .Replace(" bags", "")
+                .Replace(" bag", "")
+                .Split(", ")
+                .ToList();
+
+            foreach (var content in contents)
+            {
+                var matches = Regex.Match(content, @"^(\d+) (.+)$");
+
+                if (matches.Success)
+                {
+                    var count = int.Parse(matches.Groups[1].Value);
+                    var bagType = matches.Groups[2].Value;
+                    possibleContents.Add(translationTable[bagType], count);
+                }
+            }
+
+            return possibleContents;
+        }
+
+        public static string GetSubjectBagType(this string rule)
+        {
+            return Regex
+                .Match(rule, @"^([\w\s]+) bags contain")
+                .Groups[1]
+                .Value;
+        }
     }
 }
--- 2020/src/Day07/Solution.cs
+++ 2020/src/Day07/Solution.cs
@@ -1,11 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using McMaster.Extensions.CommandLineUtils;
 
 namespace AdventOfCode_2020.Day07
 {
-    [Command("day-7", Description = "?")]
+    [Command("day-7", Description = "Handy Haversacks")]
     public class Solution : Puzzle
     {
         public Solution(CommandLineApplication app) : base(app)
@@ -16,9 +17,24 @@ namespace AdventOfCode_2020.Day07
         {
             var inputProvider = new InputProvider<string>(7, 1);
             var inputs = inputProvider.GetInputs();
+            var translationTable = inputs.DetermineBagTypes();
+            var bagProperties = inputs.DetermineBagContentsWith(translationTable);
+            var shinyGoldBagId = translationTable["shiny gold"];
+
+            WriteVerboseLine($"Found {translationTable.Count} colors of bags.");
+            WriteVerboseLine($"Shiny gold bag has ID {shinyGoldBagId}");
+
+            var results = new Dictionary<int, bool>();
+
+            foreach (var bag in bagProperties)
+            {
+                results[bag.Key] = CanHold(bag.Key, shinyGoldBagId, bagProperties, results);
+            }
+
+            var bagCount = results.Count(result => result.Value);
 
             WriteHeaderLine("Day 7; part 1");
-            WriteSuccessLine($"?");
+            WriteSuccessLine($"There are {bagCount} bags that can hold a shiny gold bag.");
             WriteLine();
 
             WriteHeaderLine("Day 7; part 2");
@@ -26,6 +42,28 @@ namespace AdventOfCode_2020.Day07
             WriteLine();
 
             return Task.FromResult(0);
+        }
+
+        private static bool CanHold(int currentBag, int requiredBag, IDictionary<int, IDictionary<int, int>> properties, IDictionary<int, bool> result)
+        {
+            if (result.ContainsKey(currentBag))
+            {
+                return result[currentBag];
+            }
+
+            if (properties[currentBag].Count == 0)
+            {
+                //result[currentBag] = false;
+                return false;
+            }
+
+            if (properties[currentBag].ContainsKey(requiredBag))
+            {
+                //result[currentBag] = true;
+                return true;
+            }
+
+            return properties[currentBag].Any(subBag => CanHold(subBag.Key, requiredBag, properties, result));
         }
     }
 }
