f0100448869a12c4c804c3a84897f6c7afdb3a75
Basje <Basje@users.noreply.github.com>
2021-12-16 16:21:20 +0100

    Add day 9 solution for part 1

---
+++ 2021.Tests/Day09Tests.cs
@@ -0,0 +1,43 @@
+﻿using Basje.AdventOfCode.Y2021.D09;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Basje.AdventOfCode.Y2021.Tests
+{
+    
+    public class Day09Tests
+    {
+        [Fact]
+        public void Part1_Should_Return_15_For_Example_Input()
+        {
+            var puzzle = new Day09(TestInput.Lines);
+
+            var answer = puzzle.SolvePart1();
+
+            answer.Should().Be(15.ToString());
+        }
+
+        //[Fact]
+        //public void Part2_Should_Return_X_For_Example_Input()
+        //{
+        //    var puzzle = new Day09(TestInput.Lines);
+
+        //    var answer = puzzle.SolvePart2();
+
+        //    answer.Should().Be("X");
+        //}
+
+        private static class TestInput
+        {
+            public static string[] Lines => new string[] {
+                "2199943210",
+                "3987894921",
+                "9856789892",
+                "8767896789",
+                "9899965678",
+            };
+        }
+    }
+}
---
+++ 2021/09/Day09.cs
@@ -0,0 +1,70 @@
+﻿namespace Basje.AdventOfCode.Y2021.D09
+{
+    public class Day09 : ISolution
+    {
+        private readonly int[,] heightmap;
+        private int MatrixWidth => heightmap.GetLength(0);
+        private int MatrixHeight => heightmap.GetLength(1);
+
+        public Day09(IEnumerable<string> input)
+        {
+            var matrixWidth = input.First().Length;
+            var matrixHeight = input.Count();
+
+            heightmap = new int[matrixWidth, matrixHeight];
+
+            for (var x = 0; x < matrixWidth; x++)
+            {
+                for (var y = 0; y < matrixHeight; y++)
+                {
+                    heightmap[x, y] = int.Parse(input.ElementAt(y)[x].ToString());
+                }
+            }
+        }
+
+        public string SolvePart1()
+        {
+            var totalRiskLevel = 0;
+
+            for (var x = 0; x < MatrixWidth; x++)
+            {
+                for (var y = 0; y < MatrixHeight; y++)
+                {
+                    if (IsLowPoint(x, y))
+                    {
+                        totalRiskLevel += heightmap[x, y] + 1;
+                    }
+                }
+            }
+            return totalRiskLevel.ToString();
+        }
+
+        public string SolvePart2()
+        {
+            return string.Empty;
+        }
+
+        private bool IsLowPoint(int x, int y)
+        {
+            var isLowestPoint = true;
+
+            for (var deltaX = -1; deltaX <= 1; deltaX++)
+            {
+                for (var deltaY = -1; deltaY <= 1; deltaY++)
+                {
+                    var checkX = x + deltaX;
+                    var checkY = y + deltaY;
+
+                    if (checkX < 0 || checkY < 0) continue;
+                    if (checkX >= MatrixWidth || checkY >= MatrixHeight) continue;
+                    if (checkX == x && checkY == y) continue;
+                    if (checkX != x && checkY != y) continue;
+
+                    isLowestPoint = isLowestPoint && heightmap[x, y] < heightmap[checkX, checkY];
+                }
+            }
+
+            return isLowestPoint;
+        }
+    }
+}
