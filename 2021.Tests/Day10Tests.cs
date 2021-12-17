7a5d23a9ba5ea2f5ed386772a2980dad1f15f6f0
Basje <Basje@users.noreply.github.com>
2021-12-17 23:02:08 +0100

    Add day 10 solution for part 1

---
+++ 2021.Tests/Day10Tests.cs
@@ -0,0 +1,48 @@
+﻿using Basje.AdventOfCode.Y2021.D10;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Basje.AdventOfCode.Y2021.Tests
+{
+    
+    public class Day10Tests
+    {
+        [Fact]
+        public void Part1_Should_Return_26397_For_Example_Input()
+        {
+            var puzzle = new Day10(TestInput.Lines);
+
+            var answer = puzzle.SolvePart1();
+
+            answer.Should().Be(26397.ToString());
+        }
+
+        //[Fact]
+        //public void Part2_Should_Return_X_For_Example_Input()
+        //{
+        //    var puzzle = new Day10(TestInput.Lines);
+
+        //    var answer = puzzle.SolvePart2();
+
+        //    answer.Should().Be("X");
+        //}
+
+        private static class TestInput
+        {
+            public static string[] Lines => new string[] {
+                "[({(<(())[]>[[{[]{<()<>>",
+                "[(()[<>])]({[<{<<[]>>(",
+                "{([(<{}[<>[]}>{[]{[(<()>",
+                "(((({<>}<{<{<>}{[]{[]{}",
+                "[[<[([]))<([[{}[[()]]]",
+                "[{[{({}]{}}([{[{{{}}([]",
+                "{<[[]]>}<{[{[{[]{()[[[]",
+                "[<(<(<(<{}))><([]([]()",
+                "<{([([[(<>()){}]>(<<{{",
+                "<{([{{}}[<[[[<>{}]]]>[]]",
+            };
+        }
+    }
+}
---
+++ 2021/10/Day10.cs
@@ -0,0 +1,68 @@
+﻿namespace Basje.AdventOfCode.Y2021.D10
+{
+    public class Day10 : ISolution
+    {
+        private readonly IEnumerable<string> navigationSubsystem;
+        private readonly IDictionary<char, char> pairs = new Dictionary<char, char>()
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' },
+            { '>', '<' },
+        };
+        private readonly IDictionary<char, int> characterValues = new Dictionary<char, int>()
+        {
+            { ')', 3 },
+            { ']', 57 },
+            { '}', 1197 },
+            { '>', 25137 },
+        };
+
+        public Day10(IEnumerable<string> input)
+        {
+            navigationSubsystem = input;
+        }
+
+        public string SolvePart1()
+        {
+            var errorCharacters = new List<char>();
+
+            foreach (var line in navigationSubsystem)
+            {
+                var chunckCharacters = new Stack<char>();
+
+                foreach (var character in line)
+                {
+                    if (pairs.ContainsKey(character))
+                    {
+                        var currentChunkCharacter = chunckCharacters.Pop();
+
+                        if (!pairs[character].Equals(currentChunkCharacter))
+                        {
+                            errorCharacters.Add(character);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        chunckCharacters.Push(character);
+                    }
+                }
+            }
+
+            var syntaxErrorScore = 0;
+
+            foreach (var character in errorCharacters)
+            {
+                syntaxErrorScore += characterValues[character];
+            }            
+
+            return syntaxErrorScore.ToString();
+        }
+
+        public string SolvePart2()
+        {
+            return string.Empty;
+        }
+    }
+}
