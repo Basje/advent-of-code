27d3d0933a2ed550ec4e1af454027bc0605a3a34
Basje <Basje@users.noreply.github.com>
2022-12-01 16:12:18 +0100

    Set up for year 2022

---
+++ 2022/01/Day01.cs
@@ -0,0 +1,27 @@
+﻿namespace Basje.AdventOfCode.Y2022.D01
+{
+    public class Day01 : ISolution
+    {
+        private readonly IEnumerable<int> depths;
+
+        public Day01(IEnumerable<string> input)
+        {
+            depths = input.Select(ToInt);
+        }
+
+        public string SolvePart1()
+        {
+            return string.Empty;
+        }
+
+        public string SolvePart2()
+        {
+            return string.Empty;
+        }
+
+        static int ToInt(string input)
+        {
+            return int.Parse(input);
+        }
+    }
+}
---
+++ 2022/ISolution.cs
@@ -0,0 +1,10 @@
+﻿namespace Basje.AdventOfCode.Y2022
+{
+    public interface ISolution
+    {
+        public string SolvePart1();
+
+        public string SolvePart2();
+
+    }
+}
