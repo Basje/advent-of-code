db6f0424a6d7f83cba1b6ac990578575e9e3dbd9
Basje <Basje@users.noreply.github.com>
2022-12-02 14:41:44 +0100

    Solve 2022 day 2

---
+++ 2022/02/Day02.cs
@@ -0,0 +1,33 @@
+﻿namespace Basje.AdventOfCode.Y2022.D02;
+
+public class Day02 : ISolution
+{
+    private readonly IEnumerable<(char opponent, char me)> strategyGuide;
+
+    public Day02(string input)
+    {
+        strategyGuide = input
+            // Split into separate instructions
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            // Split into opponent's play and our answer
+            .Select(instruction => instruction.Split(' ').Select(char.Parse))
+            // Convert to tuple
+            .Select(instruction => (opponent: instruction.First(), me: instruction.Last()));
+    }
+
+    public long SolvePart1()
+    {
+        return strategyGuide
+            .Select(instruction => instruction.TranslateFromChoice())
+            .Select(instruction => instruction.MatchScore() + instruction.me.ShapeScore())
+            .Sum();
+    }
+
+    public long SolvePart2()
+    {
+        return strategyGuide
+            .Select(instruction => instruction.TranslateFromResult())
+            .Select(instruction => instruction.me.ShapeScore() + instruction.MatchScore())
+            .Sum();
+    }
+}
