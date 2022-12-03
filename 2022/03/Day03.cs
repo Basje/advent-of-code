9e3a8d4674f51b871efe0233673cb85598a00803
Basje <Basje@users.noreply.github.com>
2022-12-03 14:41:25 +0100

    Solve 2022 day 3

---
+++ 2022/03/Day03.cs
@@ -0,0 +1,55 @@
+﻿namespace Basje.AdventOfCode.Y2022.D03;
+
+public class Day03 : ISolution
+{
+    private readonly IEnumerable<string> rucksacks;
+
+    public Day03(string input)
+    {
+        rucksacks = input
+            // Split into separate rucksacks
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public long SolvePart1()
+    {
+        return rucksacks
+            // Split rucksacks into two equal compartments
+            .Select(rucksack => (
+                left: rucksack[..(rucksack.Length / 2)],
+                right: rucksack[(rucksack.Length / 2)..])
+            )
+            // Get the item from both compartments
+            .Select(rucksack => rucksack.left.Intersect(rucksack.right).Single())
+            // Determine priority of items
+            .Select(item => item.PriorityValue())
+            // Get sum of all priorities
+            .Sum();
+    }
+
+    public long SolvePart2()
+    {
+        return rucksacks
+            // Divide into groups of three elves
+            .Chunk(3)
+            // Find the common item within each group
+            .Select(group => group[0].Intersect(group[1]).Intersect(group[2]).Single())
+            // Determine priority of items
+            .Select(item => item.PriorityValue())
+            // Get sum of all priorities
+            .Sum();
+    }
+}
+
+public static class Extensions
+{
+    public static int PriorityValue(this char item)
+    {
+        return item switch
+        {
+            >= 'a' and <= 'z' => item - 'a' + 1,
+            >= 'A' and <= 'Z' => item - 'A' + 27,
+            _ => throw new ArgumentException()
+        };
+    }
+}
