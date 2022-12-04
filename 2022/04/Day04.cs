3788e3b6c4dd82952dc84a6619b8dbe30a779db9
Basje <Basje@users.noreply.github.com>
2022-12-04 11:45:12 +0100

    Solve 2022 day 4

---
+++ 2022/04/Day04.cs
@@ -0,0 +1,47 @@
+﻿namespace Basje.AdventOfCode.Y2022.D04;
+
+public class Day04 : ISolution
+{
+    private readonly IEnumerable<(IEnumerable<int> left, IEnumerable<int> right)> assignments;
+
+    public Day04(string input)
+    {
+        assignments = input
+            // Split into separate section assignments
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            // Split into a pair of sections
+            .Select(pair => pair.Split(','))
+            // Convert to a tuple
+            .Select(sections => (left: sections.First(), right: sections.Last()))
+            // Split range of selections into start and end numbers
+            .Select(sections => (left: sections.left.Split('-').Select(int.Parse), right: sections.right.Split('-').Select(int.Parse)))
+            // Convert start and end numbers into ranges
+            .Select(sections => (
+                left: Enumerable.Range(sections.left.First(), sections.left.Last() - sections.left.First() + 1),
+                right: Enumerable.Range(sections.right.First(), sections.right.Last() - sections.right.First() + 1)
+            ));
+    }
+
+    public long SolvePart1()
+    {
+        return assignments
+            // Filter the sections that fully overlap...
+            .Where(pair => 
+                pair.left.Intersect(pair.right).Count() == pair.left.Count() ||
+                pair.right.Intersect(pair.left).Count() == pair.right.Count()
+            )
+            // ... and count them
+            .Count();
+    }
+
+    public long SolvePart2()
+    {
+        return assignments
+            // Filter the sections that partially overlap...
+            .Where(pair =>
+                pair.left.Intersect(pair.right).Any()
+            )
+            // ... and count them
+            .Count();
+    }
+}
