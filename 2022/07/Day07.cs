232ff857d21b006e03e8490c9ad0800120f79c68
Basje <Basje@users.noreply.github.com>
2022-12-08 09:30:47 +0100

    Improve readability

--- 2022/07/Day07.cs
+++ 2022/07/Day07.cs
@@ -1,76 +1,70 @@
-﻿namespace Basje.AdventOfCode.Y2022.D07;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Basje.AdventOfCode.Y2022.D07;
 
 public class Day07 : ISolution
 {
-    private const long FS_SIZE = 70_000_000;
-    private const long FS_MIN_FREE = 30_000_000;
-
-    private readonly IFilesystemNode root = new Directory { Name = "/" };
+    private readonly Directory root = new Directory { Name = "/" };
 
     public Day07(string input)
     {
+
         var terminalOutput = input
             .Split('\n', StringSplitOptions.RemoveEmptyEntries)
-            .Select(line => line.Split(' '));
+            .Select(line => line.Split(' '))
+            .Select(line => (line[0], line[1], line.Length > 2 ? line[2] : null));
 
         var currentDirectory = new Stack<Directory>();
 
         foreach (var line in terminalOutput)
         {
-            if (line[0] == "$" && line[1] == "cd" && line[2] == "/")
+            switch(line)
             {
-                currentDirectory.Clear();
-                currentDirectory.Push((Directory)root);
-                continue;
-            }
-
-            if (line[0] == "$" && line[1] == "cd" && line[2] == "..")
-            {
-                currentDirectory.Pop();
-                continue;
-            }
-
-            if (line[0] == "$" && line[1] == "cd") {
-                currentDirectory.Push(currentDirectory.Peek().GetDirectory(line[2]));
-                continue;
-            }
-
-            if (line[0] == "$" && line[1] == "ls") continue;
-
-            if (line[0] == "dir")
-            {
-                currentDirectory.Peek().Add(new Directory { Name = line[1] });
-                continue;
-            }
-
-            if (line[0].All(character => '0' <= character && character <= '9'))
-            {
-                currentDirectory.Peek().Add(new File { Name = line[1], Size = int.Parse(line[0]) });
-                continue;
-            }
-
-            throw new Exception();
+                case ("$", "cd", "/"):
+                    currentDirectory.Push(root);
+                    break;
+                case ("$", "cd", ".."):
+                    currentDirectory.Pop();
+                    break;
+                case ("$", "cd", _):
+                    currentDirectory.Push(currentDirectory.Peek().GetDirectory(line.Item3)!);
+                    break;
+                case ("$", "ls", _):
+                    break;
+                case ("dir", _, _):
+                    currentDirectory.Peek().Add(new Directory { Name = line.Item2 });
+                    break;
+                case (_, _, _) when line.Item1.All(character => '0' <= character && character <= '9'):
+                    currentDirectory.Peek().Add(new File { Name = line.Item2, Size = int.Parse(line.Item1) });
+                    break;
+                case (_, _, _):
+                    throw new Exception();
+            };
         }
     }
 
     public object SolvePart1()
     {
-        return ((Directory)root)
+        return root
             .Find(dir => dir.Size <= 100_000)
             .Sum(dir => dir.Size);
     }
 
     public object SolvePart2()
     {
+        const long FS_SIZE = 70_000_000;
+        const long FS_MIN_FREE = 30_000_000;
+
         var currentlyFree = FS_SIZE - root.Size;
         var requiredCleanup = FS_MIN_FREE - currentlyFree;
 
-        return ((Directory)root)
+        return root
             .Find(dir => dir.Size >= requiredCleanup)
             .Min(dir => dir.Size);
     }
 }
 
+// Common interface so that I can put all nodes in the same collection
 public interface IFilesystemNode
 {
     public long Size { get;  }
@@ -78,16 +72,24 @@ public interface IFilesystemNode
     public string Name { get; }
 }
 
-public record Directory : IFilesystemNode
+// Files are dumb data records
+public record File : IFilesystemNode
 {
-    private List<IFilesystemNode> contents = new();
-
-    public long Size => contents.Select(node => node.Size).Sum();
+    public required long Size { get; init; }
 
     public required string Name { get; init; }
+}
+
+// Directories have some added logic on top of the data
+public record Directory : IFilesystemNode
+{
+    private readonly List<IFilesystemNode> contents = new();
+
+    public required string Name { get; init; }
+    public long Size => contents.Select(node => node.Size).Sum();
+
 
     public void Add(IFilesystemNode node) => contents.Add(node);
-
     public Directory? GetDirectory(string name) => contents.Single(entry => entry is Directory && entry.Name == name) as Directory;
 
     public IEnumerable<Directory> Find(Func<Directory, bool> check)
@@ -103,7 +105,7 @@ public record Directory : IFilesystemNode
 
             foreach (var dir in current.contents.Where(d => d is Directory).Select(d => d as Directory))
             {
-                directories.Push(dir);
+                directories.Push(dir!);
             }
 
             if (check(current))
@@ -116,11 +118,5 @@ public record Directory : IFilesystemNode
     }
 }
 
-public record File : IFilesystemNode
-{
-    public required long Size { get; init; }
-
-    public required string Name { get; init; }
-}
 
 
