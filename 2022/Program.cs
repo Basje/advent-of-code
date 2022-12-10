2af0b3319cf3075ebcca9f2532ba9cc909f9f586
Basje <Basje@users.noreply.github.com>
2022-12-10 17:36:29 +0100

    Change output layout
    Improve readability for 2022 day 10 part 2 output

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -14,9 +14,16 @@ foreach (var solution in solutions)
     var day = int.Parse(solution.Name.Replace("Day", ""));
     var instance = (ISolution?)Activator.CreateInstance(solution, args: Input.Year(2022).Day(day).ReadText());
 
-    Console.WriteLine(solution.Name.ToUpper());
-    Console.WriteLine($"1: {instance?.SolvePart1()}");
-    Console.WriteLine($"2: {instance?.SolvePart2()}");
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine($"{solution.Name.ToUpper()}.1");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(instance?.SolvePart1());
+    Console.WriteLine();
+
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine($"{solution.Name.ToUpper()}.2");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(instance?.SolvePart2());
     Console.WriteLine();
 }
 
