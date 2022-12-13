5a5e31090d09e098590f9ff525dc27ef3b7d2b1e
Basje <Basje@users.noreply.github.com>
2022-12-13 20:17:47 +0100

    Clean up code

--- 2022/13/Day13.cs
+++ 2022/13/Day13.cs
@@ -18,9 +18,9 @@ public class Day13 : ISolution
         var pairNumber = 1;
         var orderedPairs = new List<int>();
 
-        foreach (var pair in packets)
+        foreach (var (left, right) in packets)
         {
-            if(IsInOrder(pair.left, pair.right, 0, 0, 0))
+            if(IsInOrder(left, right, 0, -1, -1))
             {
                 orderedPairs.Add(pairNumber);
             }
@@ -31,7 +31,8 @@ public class Day13 : ISolution
 
     public object SolvePart2()
     {
-        var allPackets = new List<string>() { "[[2]]", "[[6]]" };
+        var dividerPackets = new string[] { "[[2]]", "[[6]]" };
+        var allPackets = new List<string>();
 
         foreach (var (left, right) in packets)
         {
@@ -45,78 +46,61 @@ public class Day13 : ISolution
 
         foreach(var packet in sorted)
         {
-            if (new string[] {"[[2]]", "[[6]]"}.Contains(packet))
+            if (dividerPackets.Contains(packet))
             {
                 dividerPacketLocations.Add(searchLocation);
             }
             searchLocation++;
         }
 
-        var decoderKey = 1;
-
-        foreach(var location in  dividerPacketLocations)
-        {
-            decoderKey *= location;
-        }
-
-        return decoderKey;
+        return dividerPacketLocations.Aggregate((left, right) => left * right); ;
     }
 
     // Recursive function to walk through both packets simultaneously
     public static bool IsInOrder(string left, string right, int level, int lNumber, int rNumber)
     {
+        const int NONE = 1;
+
         if (level == 0 && (left.Length == 0 || right.Length == 0)) return true;
 
         return (left: left[0], right: right[0]) switch
         {
-            // Exit case: left has run out of items
+            // Exit: left has run out of items
             (']', '[') or (']', ',') => lNumber == rNumber || lNumber < rNumber,
-
-            // Exit case: right has run out of items
+            // Exit: right has run out of items
             ('[', ']') or (',', ']') => lNumber != rNumber && lNumber < rNumber,
+            // Exit: compare different items in packets
+            (']', ']') or (',', ',') when lNumber != rNumber => lNumber < rNumber,
 
-            // Possible exit case: compare last items in packets
-            (']', ']')
-                => lNumber == rNumber
-                    ? IsInOrder(left.Next(), right.Next(), --level, -1, -1)
-                    : lNumber < rNumber,
-
-            // Possible exit case: compare items in packets
-            (',', ',')
-                => lNumber == rNumber
-                    ? IsInOrder(left.Next(), right.Next(), level, -1, -1)
-                    : lNumber < rNumber,
-
-            // New list in both packets
-            ('[', '[')
-                => IsInOrder(left.Next(), right.Next(), ++level, -1, -1),
-
-            // Left is next digit of current number, right is ready for next number
-            (_, ',') token when token.left.IsDigit()
+            // Continue: so far still the same, go up a level
+            (']', ']') when lNumber == rNumber 
+                => IsInOrder(left.Next(), right.Next(), --level, NONE, NONE),
+            // Continue: so far still the same, stay in level
+            (',', ',') when lNumber == rNumber 
+                => IsInOrder(left.Next(), right.Next(), level, NONE, NONE),
+            // Continue: new list in both packets, go down a level
+            ('[', '[') 
+                => IsInOrder(left.Next(), right.Next(), ++level, NONE, NONE),
+            // Continue: left is next digit of current number, right is ready for next number
+            (>= '0' and <= '9', ',') token 
                 => IsInOrder(left.Next(), right, level, lNumber.Add(token.left), rNumber),
-
-            // Left is next digit of current number, right is end of list
-            (_, ']') token when token.left.IsDigit()
-                => rNumber < 0 ? false : IsInOrder(left.Next(), right, level, lNumber.Add(token.left), rNumber),
-
-            // Left is ready for next number, right is next digit of current number
-            (',', _) token when token.right.IsDigit()
+            // Continue: left is next digit of current number, right is end of list
+            (>= '0' and <= '9', ']') token 
+                => rNumber >= 0 && IsInOrder(left.Next(), right, level, lNumber.Add(token.left), rNumber),
+            // Continue: left is ready for next number, right is next digit of current number
+            (',', >= '0' and <= '9') token 
                 => IsInOrder(left, right.Next(), level, lNumber, rNumber.Add(token.right)),
-
-            // Left is end of list, right is next digit of current number
-            (']', _) token when token.right.IsDigit()
-                => lNumber < 0 ? true : IsInOrder(left, right.Next(), level, lNumber, rNumber.Add(token.right)),
-
-            // Mixed types, right is plain number while left is new list
-            ('[', _) token when token.right.IsDigit()
-                => IsInOrder(left, right.NumberToList(), level, -1, -1),
-
-            // Mixed types, left is plain number while right is new list
-            (_, '[') token when token.left.IsDigit()
-                => IsInOrder(left.NumberToList(), right, level, -1, -1),
-
-            // Digits, track numbers and keep parsing
-            (_, _) token when token.left.IsDigit() && token.right.IsDigit()
+            // Continue: left is end of list, right is next digit of current number
+            (']', >= '0' and <= '9') token 
+                => lNumber == NONE || IsInOrder(left, right.Next(), level, lNumber, rNumber.Add(token.right)),
+            // Continue: mixed types, right is plain number while left is new list
+            ('[', >= '0' and <= '9') token 
+                => IsInOrder(left, right.NumberToList(), level, NONE, NONE),
+            // Continue: mixed types, left is plain number while right is new list
+            (>= '0' and <= '9', '[') token 
+                => IsInOrder(left.NumberToList(), right, level, NONE, NONE),
+            // Continue: digits, track numbers and keep parsing 
+            ( >= '0' and <= '9', >= '0' and <= '9') token
                 => IsInOrder(left.Next(), right.Next(), level, lNumber.Add(token.left), rNumber.Add(token.right)),
 
             // Are we done debugging yet?
@@ -129,59 +113,38 @@ public class PacketComparer : StringComparer
 {
     public override int Compare(string? left, string? right)
     {
-        if (left is null && right is null) return 0;
-        if (left is null) return -1;
-        if (right is null) return 1;
-
-        return Day13.IsInOrder(left, right, 0, -1, -1) ? -1 : 1;
+        return (left, right) switch
+        {
+            (null, null) => 0,
+            (null, _) => -1,
+            (_, null) => 1,
+            (_, _) => Day13.IsInOrder(left, right, 0, -1, -1) ? -1 : 1,
+        };
     }
 
-    public override bool Equals(string? left, string? right)
-    {
-        return Compare(left, right) == 0;
-    }
+    public override bool Equals(string? left, string? right) => Compare(left, right) == 0;
 
-    public override int GetHashCode(string obj)
-    {
-        return obj.GetHashCode();
-    }
+    public override int GetHashCode(string obj) => obj.GetHashCode();
 }
 
 public static class Extensions
 {
-    public static bool IsDigit(this char character)
-    {
-        return character >= '0' && character <= '9';
-    }
+    public static string Next(this string packet) => packet[1..];
 
-    public static string Next(this string packet)
+    public static int Add(this int number, char next) => number switch
     {
-        return packet[1..];
-    }
-
-    public static int Add(this int number, char next)
-    {
-        return number < 0 ? next - '0' : number * 10 + (next - '0');
-    }
+        < 0 => next - '0',
+        _ => number * 10 + (next - '0'),
+    };
 
     public static string NumberToList(this string packet)
     {
-        var nextComma = packet.IndexOf(',');
-        var nextOpeningBracket = packet.IndexOf("[");
-        var nextClosingBracket = packet.IndexOf("]");
+        var insertAt = packet.IndexOfAny(new char[] { ',', '[', ']' });
 
-        var indices = new int[] { 
-            nextComma < 0 ? int.MaxValue : nextComma,
-            nextOpeningBracket < 0 ? int.MaxValue : nextOpeningBracket,
-            nextClosingBracket < 0 ? int.MaxValue : nextClosingBracket,
-        };
-
-        var newClosingBracketLocation = indices.Min();
-
-        return newClosingBracketLocation switch
+        return insertAt switch
         {
-            int.MaxValue => '[' + packet + ']',
-            _ => "[" + packet[..newClosingBracketLocation] + "]" + packet[newClosingBracketLocation..],
+            -1 => '[' + packet + ']',
+            _ => "[" + packet[..insertAt] + "]" + packet[insertAt..],
         };
     }
 }
