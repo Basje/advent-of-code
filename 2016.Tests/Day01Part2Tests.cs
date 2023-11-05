fa62e95c371a88d9c4463b61f2fb377fc8319542
Basje <Basje@users.noreply.github.com>
2023-11-05 17:31:22 +0100

    Solve 2016 day 1 part 2

---
+++ 2016.Tests/Day01Part2Tests.cs
@@ -0,0 +1,23 @@
+using Basje.AdventOfCode.Y2016.D01;
+using FluentAssertions;
+
+namespace Tests;
+
+public class Day01Part2Tests
+{
+    [Fact]
+    public void Example1()
+    {
+        var solution = new Day01();
+        var answer = solution.SolvePart2("R8, R4, R4, R8");
+        answer.Should().Be(4);
+    }
+    
+    [Fact]
+    public void Answer()
+    {
+        var solution = new Day01();
+        var answer = solution.SolvePart2("R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2");
+        answer.Should().Be(147);
+    }
+}
--- 2016/01/Day01.cs
+++ 2016/01/Day01.cs
@@ -1,8 +1,16 @@
 ﻿namespace Basje.AdventOfCode.Y2016.D01;
 
-public class Day01 : Solution<IEnumerable<(char direction, int distance)>>
+public class Day01 : Solution<IEnumerable<(char turn, int distance)>>
 {
-    protected override IEnumerable<(char direction, int distance)> ParseInput(string input)
+    private const char NORTH = 'N';
+    private const char EAST = 'E';
+    private const char SOUTH = 'S';
+    private const char WEST = 'W';
+
+    private const char LEFT = 'L';
+    private const char RIGHT = 'R';
+    
+    protected override IEnumerable<(char turn, int distance)> ParseInput(string input)
     {
         return input
             // Separate the different commands    
@@ -11,39 +19,72 @@ public class Day01 : Solution<IEnumerable<(char direction, int distance)>>
             .Select(command => (direction: command[0], distance: int.Parse(command[1..].ToString())));
     }
     
-    protected override object SolvePart1(IEnumerable<(char direction, int distance)> input)
+    protected override object SolvePart1(IEnumerable<(char turn, int distance)> input)
     {
         var currentPosition = (x: 0, y: 0);
-        var direction = 'N';
+        var direction = NORTH;
         
         foreach (var command in input)
         {
-            direction = (direction, command.direction) switch
-            {
-                ('N', 'L') => 'W',
-                ('N', 'R') => 'E',
-                ('E', 'L') => 'N',
-                ('E', 'R') => 'S',
-                ('S', 'L') => 'E',
-                ('S', 'R') => 'W',
-                ('W', 'L') => 'S',
-                ('W', 'R') => 'N',
-            };
-            
-            currentPosition = direction switch
-            {
-                'N' => (currentPosition.x, currentPosition.y + command.distance),
-                'E' => (currentPosition.x + command.distance, currentPosition.y),
-                'S' => (currentPosition.x, currentPosition.y - command.distance),
-                'W' => (currentPosition.x - command.distance, currentPosition.y),
-            };
+            direction = Turn(direction, command.turn);
+            currentPosition = NextPosition(direction, currentPosition, command.distance);
         }
         
         return Math.Abs(currentPosition.x) + Math.Abs(currentPosition.y);
     }
 
-    protected override object SolvePart2(IEnumerable<(char direction, int distance)> input)
+    protected override object SolvePart2(IEnumerable<(char turn, int distance)> input)
     {
-        return -1;
+        var currentPosition = (x: 0, y: 0);
+        var endOfSegment = (x: 0, y: 0);
+        var direction = NORTH;
+        var visitedPositions = new List<(int x, int y)> { currentPosition };
+        
+        foreach (var command in input)
+        {
+            
+            direction = Turn(direction, command.turn);
+            endOfSegment = NextPosition(direction, currentPosition, command.distance);
+
+            do
+            {
+                currentPosition = NextPosition(direction, currentPosition, 1);
+                if (visitedPositions.Contains(currentPosition))
+                {
+                    return Math.Abs(currentPosition.x) + Math.Abs(currentPosition.y);
+                }
+
+                visitedPositions.Add(currentPosition);
+            } while (currentPosition != endOfSegment);
+        }
+        
+        return Math.Abs(currentPosition.x) + Math.Abs(currentPosition.y);
+    }
+    
+    private static char Turn(char fromDirection, char turnTo)
+    {
+        return (fromDirection, turnTo) switch
+        {
+            (NORTH, LEFT) => WEST,
+            (NORTH, RIGHT) => EAST,
+            (EAST, LEFT) => NORTH,
+            (EAST, RIGHT) => SOUTH,
+            (SOUTH, LEFT) => EAST,
+            (SOUTH, RIGHT) => WEST,
+            (WEST, LEFT) => SOUTH,
+            (WEST, RIGHT) => NORTH,
+            (_, _) => throw new ArgumentException($"Invalid direction or turn: {fromDirection}, {turnTo}"),
+        };
+    }
+
+    private static (int x, int y) NextPosition(char direction, (int x, int y) currentPosition, int distance)
+    {
+        return direction switch
+        {
+            NORTH => (currentPosition.x, currentPosition.y + distance),
+            EAST => (currentPosition.x + distance, currentPosition.y),
+            SOUTH => (currentPosition.x, currentPosition.y - distance),
+            WEST => (currentPosition.x - distance, currentPosition.y),
+        };
     }
 }
