2cc64b08e9ec57c4023b8c9b5325260ee34c4985
Basje <Basje@users.noreply.github.com>
2023-12-01 11:39:46 +0100

    Solve 2023 day 1

--- 2023/01/Day01.cs
+++ 2023/01/Day01.cs
@@ -1,19 +1,60 @@
-﻿namespace Basje.AdventOfCode.Y2023.D01;
+﻿using System.Text.RegularExpressions;
+using Basje.AdventOfCode.Y2023.Input;
 
-public class Day01 : Solution<int>
+namespace Basje.AdventOfCode.Y2023.D01;
+
+public class Day01 : Solution<IEnumerable<string>>
 {
-    protected override int ParseInput(string input)
+    protected override IEnumerable<string> ParseInput(string input)
     {
-        return -1;
+        return input
+            .PerLine()
+            .IgnoreEmptyLines();
     }
     
-    protected override object SolvePart1(int input)
+    protected override object SolvePart1(IEnumerable<string> input)
     {
-        return input;
+        var lines = input
+            .Select(line => Regex.Replace(line, "[a-z]", ""));
+
+        var total = 0;
+        
+        foreach (var line in lines)
+        {
+            if (line.Length == 0) continue;
+            var number = int.Parse($"{line.First()}{line.Last()}");
+            total += number;
+        }
+        
+        return total;
     }
 
-    protected override object SolvePart2(int input)
+    protected override object SolvePart2(IEnumerable<string> input)
     {
-        return input;
-    }
+               
+
+            // .Select(line => Regex.Replace(line, "[a-z]", ""));
+
+        var total = 0;
+        
+        foreach (var line in input)
+        {
+            var line2 = line
+                .Replace("one", "o1e")
+                .Replace("two", "t2o")
+                .Replace("three", "thr3e")
+                .Replace("four", "f4ur")
+                .Replace("five", "f5ve")
+                .Replace("six", "s6x")
+                .Replace("seven", "se7en")
+                .Replace("eight", "eih8ht")
+                .Replace("nine", "n9ne");
+            
+            line2 = Regex.Replace(line2, "[a-z]", "");
+            
+            var number = int.Parse($"{line2.First()}{line2.Last()}");
+            total += number;
+        }
+        
+        return total;    }
 }
