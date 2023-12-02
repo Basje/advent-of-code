23c61ec7d38e2fae4c303d1fb2010200f7f3062b
Basje <Basje@users.noreply.github.com>
2023-12-02 09:48:06 +0100

    Solve 2023 day 2

---
+++ 2023/02/Game.cs
@@ -0,0 +1,7 @@
+﻿namespace Basje.AdventOfCode.Y2023.D02;
+
+public record Game()
+{
+    public int Id { init; get; }
+    public IEnumerable<Set> Sets { init; get; }
+}
---
+++ 2023/02/Set.cs
@@ -0,0 +1,6 @@
+﻿namespace Basje.AdventOfCode.Y2023.D02;
+
+public record Set()
+{
+    public IEnumerable<(string Color, int Count)> Cubes { init; get; } 
+}
