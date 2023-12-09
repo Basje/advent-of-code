7a77d477d41b51b4c9d96630f6c55eb3fe0b405f
Basje <Basje@users.noreply.github.com>
2023-12-09 22:38:05 +0100

    Solve 2023 day 6

---
+++ 2023.Tests/Day06Tests.cs
@@ -0,0 +1,41 @@
+﻿using Basje.AdventOfCode.Y2023.D06;
+
+namespace Basje.AdventOfCode.Y2023.Tests
+{
+    public static class Day06Tests
+    {
+        [Fact]
+        public static void Part1() 
+        {
+            // Arrange 
+            var solution = new Day06();
+            var input = """
+                        Time:      7  15   30
+                        Distance:  9  40  200
+                        """;
+
+            // Act
+            var answer = solution.SolvePart1(input);
+
+            // Assert
+            answer.Should().Be(288);
+        }
+
+        [Fact]
+        public static void Part2()
+        {
+            // Arrange 
+            var solution = new Day06();
+            var input = """
+                        Time:      7  15   30
+                        Distance:  9  40  200
+                        """;
+
+            // Act
+            var answer = solution.SolvePart2(input);
+
+            // Assert
+            answer.Should().Be(71503);
+        }
+    }
+}
---
+++ 2023/06/Day06.cs
@@ -0,0 +1,74 @@
+﻿using Basje.AdventOfCode.Y2023.Input;
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Basje.AdventOfCode.Y2023.D06
+{
+    public class Day06 : Solution<List<List<int>>>
+    {
+        protected override List<List<int>> ParseInput(string input)
+        {
+            return input
+                .PerLine()
+                .IgnoreEmptyLines()
+                .Select(line => Regex.Matches(line, @"\d+"))
+                .Select(line => line.Select(match => match.Value).Select(int.Parse).ToList())
+                .ToList();
+        }
+
+        protected override object SolvePart1(List<List<int>> sheet)
+        {
+            const int TIME = 0;
+            const int DISTANCE = 1;
+
+            var answer = 1;
+
+            for (var raceNumber = 0; raceNumber < sheet[TIME].Count; raceNumber++)
+            {
+                var numberOfWins = 0;
+                var time = sheet[TIME][raceNumber];
+                var distance = sheet[DISTANCE][raceNumber];
+                for (var hold = 1; hold < time; hold++)
+                {
+                    if (hold * (time - hold) > distance)
+                    {
+                        numberOfWins++;
+                    }
+                }
+                answer *= numberOfWins;
+            }
+
+            return answer;
+        }
+
+        protected override object SolvePart2(List<List<int>> sheet)
+        {
+            var timeBuilder = new StringBuilder();
+            var distanceBuilder = new StringBuilder();
+
+            foreach (var t in sheet[0])
+            {
+                timeBuilder.Append(t);
+            }
+
+            foreach (var d in sheet[1])
+            {
+                distanceBuilder.Append(d);
+            }
+
+            var time = long.Parse(timeBuilder.ToString());
+            var distance = long.Parse(distanceBuilder.ToString());
+
+            var numberOfWins = 0;
+            for (var hold = 1; hold < time; hold++)
+            {
+                if (hold * (time - hold) > distance)
+                {
+                    numberOfWins++;
+                }
+            }
+            return numberOfWins;
+        }
+    }
+}
