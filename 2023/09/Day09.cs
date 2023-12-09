9b65ffe1cd7d05907f29b56b05a69ed18e5e781d
Basje <Basje@users.noreply.github.com>
2023-12-09 15:38:28 +0100

    Solve 2023 day 9

---
+++ 2023/09/Day09.cs
@@ -0,0 +1,79 @@
+﻿using Basje.AdventOfCode.Y2023.Input;
+
+namespace Basje.AdventOfCode.Y2023.D09
+{
+    public class Day09 : Solution<Report>
+    {
+        protected override Report ParseInput(string input)
+        {
+            var histories = input
+                .PerLine()
+                .IgnoreEmptyLines()
+                .Select(line => line.Split(' ').Select(int.Parse).ToList());
+
+            return new Report() { Histories = histories };
+        }
+
+        protected override object SolvePart1(Report report)
+        {
+            var sum = 0;
+
+            foreach (var history in report.Histories)
+            {
+                var sequences = new Stack<IList<int>>();
+                var nextSequence = GetDifferencesList(history).ToList();
+
+                while (nextSequence.Any(value => value != 0)) 
+                {
+                    sequences.Push(nextSequence.ToList());
+                    nextSequence = GetDifferencesList(nextSequence).ToList();
+                }
+
+                var difference = sequences.Pop().Last();
+
+                while (sequences.Count > 0)
+                {
+                    difference += sequences.Pop().Last();
+                }
+                
+                sum += difference + history.Last();
+            }
+            return sum;
+        }
+
+        protected override object SolvePart2(Report report)
+        {
+            var sum = 0;
+
+            foreach (var history in report.Histories)
+            {
+                var sequences = new Stack<IList<int>>();
+                var nextSequence = GetDifferencesList(history).ToList();
+
+                while (nextSequence.Any(value => value != 0))
+                {
+                    sequences.Push(nextSequence.ToList());
+                    nextSequence = GetDifferencesList(nextSequence).ToList();
+                }
+
+                var difference = sequences.Pop().First();
+
+                while (sequences.Count > 0)
+                {
+                    difference = sequences.Pop().First() - difference;
+                }
+
+                sum += history.First() - difference;
+            }
+            return sum;
+        }
+
+        private static IEnumerable<int> GetDifferencesList(IList<int> sequence) 
+        {
+            for (var i = 1; i < sequence.Count; i++)
+            {
+                yield return sequence[i] - sequence[i - 1];
+            }
+        }
+    }
+}
---
+++ 2023/09/Report.cs
@@ -0,0 +1,7 @@
+﻿namespace Basje.AdventOfCode.Y2023.D09
+{
+    public record Report
+    {
+        public required IEnumerable<IList<int>> Histories { init; get; }
+    }
+}
