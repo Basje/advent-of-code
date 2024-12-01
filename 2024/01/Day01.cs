08b97b1e03306ce102d4dc3d7d03774e07e43fe2
Basje <Basje@users.noreply.github.com>
2024-12-01 19:07:19 +0100

    Rewrite 2024 day 1
    Make code more compact by using extra LINQ statements

--- 2024/01/Day01.cs
+++ 2024/01/Day01.cs
@@ -1,61 +1,35 @@
-﻿using Basje.AdventOfCode.Y2024.Input;
+﻿using System.Text.RegularExpressions;
+using Basje.AdventOfCode.Y2024.Input;
 
 namespace Basje.AdventOfCode.Y2024.D01;
 
-public class Day01 : Solution<IEnumerable<(int Left, int Right)>>
+public class Day01 : Solution<(int Left, int Right)[]>
 {
-    protected override IEnumerable<(int Left, int Right)> ParseInput(string input)
+    protected override (int Left, int Right)[] ParseInput(string input)
     {
         return input
             .PerLine()
             .IgnoreEmptyLines()
-            .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-            .Select(line => (Left: int.Parse(line[0]), Right: int.Parse(line[1])));
+            .SelectMany(line => line.Split("   "))
+            .Select(int.Parse)
+            .Chunk(2)
+            .Select(pair => (Left: pair[0], Right: pair[1]))
+            .ToArray();
     }
     
-    protected override object SolvePart1(IEnumerable<(int Left, int Right)> input)
+    protected override object SolvePart1((int Left, int Right)[] input)
     {
-        List<int> leftList = [];
-        List<int> rightList = [];
+        var left = input.Select(pair => pair.Left).Order();
+        var right = input.Select(pair => pair.Right).Order();
 
-        input.ToList().ForEach(line =>
-        {
-            leftList.Add(line.Left);
-            rightList.Add(line.Right);
-        });
-
-        var orderedLeftList = leftList.Order().ToArray();
-        var orderedRightList = rightList.Order().ToArray();
-
-        List<int> distances = [];
-
-        for (var index = 0; index < orderedLeftList.Length && index < orderedRightList.Length; index++)
-        {
-            distances.Add(Math.Abs(orderedLeftList[index] - orderedRightList[index] ));
-        }
-
-        return distances.Sum();
+        return left.Zip(right, (l, r) => Math.Abs(l - r)).Sum();
     }
 
-    protected override object SolvePart2(IEnumerable<(int Left, int Right)> input)
+    protected override object SolvePart2((int Left, int Right)[] input)
     {
-        List<int> leftList = [];
-        List<int> rightList = [];
+        var left = input.Select(pair => pair.Left);
+        var right = input.Select(pair => pair.Right);
 
-        input.ToList().ForEach(line =>
-        {
-            leftList.Add(line.Left);
-            rightList.Add(line.Right);
-        });
-
-        List<int> similarityScore = [];
-        
-        leftList.ForEach(number =>
-        {
-            var occurrences = rightList.Count(entry => entry == number);
-            similarityScore.Add(number * occurrences);
-        });
-        
-        return similarityScore.Sum();
+        return left.Aggregate(0, (similarity, number) => similarity + number * right.Count(r => r == number));
     }
 }
