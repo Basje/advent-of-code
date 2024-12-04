b28656c7ea5f2a1a985173c2ae929b03304462c3
Bas <Basje@users.noreply.github.com>
2024-12-04 07:37:54 +0100

    Fix 2024 day 1 tests

--- 2024.Tests/Day01Tests.cs
+++ 2024.Tests/Day01Tests.cs
@@ -2,7 +2,7 @@ using Basje.AdventOfCode.Y2024.D01;
 
 namespace Basje.AdventOfCode.Y2024.Tests;
 
-public abstract class Day01Tests
+public static class Day01Tests
 {
     [Fact]
     public static void Part1()
