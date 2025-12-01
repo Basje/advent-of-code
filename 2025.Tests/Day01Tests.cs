8aa3ae6c5b5d76a17154f72b6cd192495aaac725
Bas <2102620+Basje@users.noreply.github.com>
2025-12-01 09:22:53 +0100

    Solve 2025 day 1

--- 2025.Tests/Day01Tests.cs
+++ 2025.Tests/Day01Tests.cs
@@ -9,11 +9,65 @@ public static class Day01Tests
     {
         var solution = new Day01();
         const string input = """
-                             // Waiting for the first puzzle
+                             L68
+                             L30
+                             R48
+                             L5
+                             R60
+                             L55
+                             L1
+                             L99
+                             R14
+                             L82
                              """;
         
         var answer = solution.SolvePart1(input);
         
-        answer.ShouldBe("// Waiting for the first puzzle");
+        answer.ShouldBe(3);
+    }
+    
+    [Fact]
+    public static void Part2()
+    {
+        var solution = new Day01();
+        const string input = """
+                             L68
+                             L30
+                             R48
+                             L5
+                             R60
+                             L55
+                             L1
+                             L99
+                             R14
+                             L82
+                             """;
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe(6);
+    }
+    
+    [Fact]
+    public static void Part2_FullRotationEdgeCase()
+    {
+        var solution = new Day01();
+        const string input = """
+                             L68
+                             L30
+                             R48
+                             L5
+                             R60
+                             L55
+                             L1
+                             L99
+                             R14
+                             L182
+                             R1000
+                             """;
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe(17);
     }
 }
--- 2025/D01/Day01.cs
+++ 2025/D01/Day01.cs
@@ -1,19 +1,74 @@
 ﻿namespace Basje.AdventOfCode.Y2025.D01;
 
-public class Day01 : Solution<object>
+public class Day01 : Solution<IEnumerable<(char Direction, int Distance)>>
 {
-    protected override object ParseInput(string input)
+    protected override IEnumerable<(char Direction, int Distance)> ParseInput(string input)
     {
-        return input;
+        return input
+            .PerLine()
+            .IgnoreEmptyLines()
+            .Select(line => (Direction: line[0], Distance: int.Parse(line[1..])));
     }
     
-    protected override object SolvePart1(object input)
+    protected override object SolvePart1(IEnumerable<(char Direction, int Distance)> sequence)
     {
-        return input;
+        var currentValue = 50;
+        var timesAtZero = 0;
+
+        foreach (var instruction in sequence)
+        {
+            currentValue = instruction.Direction switch
+            {
+                'L' => (currentValue - instruction.Distance + 100) % 100,
+                'R' => (currentValue + instruction.Distance) % 100,
+                _ => throw new Exception($"Invalid direction: '{instruction.Direction}")
+            };
+
+            if (currentValue == 0)
+            {
+                timesAtZero++;
+            }
+        }
+        
+        return timesAtZero;
     }
 
-    protected override object SolvePart2(object input)
+    protected override object SolvePart2(IEnumerable<(char Direction, int Distance)> sequence)
     {
-        return input;
+        var previousValue = 50;
+        var currentValue = 50;
+        var timesAtZero = 0;
+
+        foreach (var instruction in sequence)
+        {
+            previousValue = currentValue;
+            currentValue = instruction.Direction switch
+            {
+                // Apparently C# % is not a true modulo, but a remainder operation
+                'L' => (((currentValue - instruction.Distance) % 100) + 100) % 100,
+                'R' => (currentValue + instruction.Distance) % 100,
+                _ => throw new Exception($"Invalid direction: '{instruction.Direction}")
+            };
+
+            var fullRotations = instruction.Distance / 100;
+            
+            timesAtZero += fullRotations;
+
+            if (currentValue == 0)
+            {
+                timesAtZero++;
+            }
+            else if (previousValue != 0)
+            {
+                timesAtZero += instruction.Direction switch
+                {
+                    'L' => currentValue > previousValue ? 1 : 0,
+                    'R' => currentValue < previousValue ? 1 : 0,
+                    _ => throw new Exception($"Invalid direction: '{instruction.Direction}'")
+                };
+            }
+        }
+        
+        return timesAtZero;
     }
 }
