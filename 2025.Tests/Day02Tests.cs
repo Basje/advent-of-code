244e781338b242f16450b8caa365b3aeb3514d10
Bas <2102620+Basje@users.noreply.github.com>
2025-12-02 08:27:11 +0100

    Solve 2025 day 2

---
+++ 2025.Tests/Day02Tests.cs
@@ -0,0 +1,28 @@
+﻿using Basje.AdventOfCode.Y2025.D02;
+
+namespace Basje.AdventOfCode.Y2025.Tests;
+
+public static class Day02Tests
+{
+    [Fact]
+    public static void Part1()
+    {
+        var solution = new Day02();
+        const string input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        
+        var answer = solution.SolvePart1(input);
+        
+        answer.ShouldBe(1227775554);
+    }
+    
+    [Fact]
+    public static void Part2()
+    {
+        var solution = new Day02();
+        const string input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe(4174379265);
+    }
+}
---
+++ 2025/D02/Day02.cs
@@ -0,0 +1,60 @@
+﻿namespace Basje.AdventOfCode.Y2025.D02;
+
+public class Day02 : Solution<IEnumerable<(long Start, long End)>>
+{
+    protected override IEnumerable<(long Start, long End)> ParseInput(string input)
+    {
+        return input
+            .Split(',')
+            .Select(line => line.Split('-'))
+            .Select(line => (Start: long.Parse(line[0]), End: long.Parse(line[1])));
+    }
+    
+    protected override object SolvePart1(IEnumerable<(long Start, long End)> ids)
+    {
+        long sum = 0;
+
+        foreach (var (start, end) in ids)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                var numberLength = i.ToString().Length;
+                var leftPart = i.ToString()[..(numberLength / 2)];
+                var rightPart = i.ToString()[(numberLength / 2)..];
+
+                if (leftPart == rightPart)
+                {
+                    sum += i;
+                }
+            }
+        }
+        
+        return sum;
+    }
+
+    protected override object SolvePart2(IEnumerable<(long Start, long End)> ids)
+    {
+        long sum = 0;
+
+        foreach (var (start, end) in ids)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                var id = i.ToString();
+                var halveNumberLength = i.ToString().Length / 2;
+
+                for (var size = halveNumberLength; size > 0; size--)
+                {
+                    var chunks = id.Chunk(size).Select(chunk => new string(chunk));
+                    if (chunks.Distinct().Count() == 1)
+                    {
+                        sum += i;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
