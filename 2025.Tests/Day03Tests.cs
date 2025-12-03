ddd1f35735512a8584dd2e7bee4265fbb6c0b927
Bas <2102620+Basje@users.noreply.github.com>
2025-12-03 07:58:39 +0100

    Solve 2025 day 3 part 1

---
+++ 2025.Tests/Day03Tests.cs
@@ -0,0 +1,38 @@
+﻿using Basje.AdventOfCode.Y2025.D03;
+
+namespace Basje.AdventOfCode.Y2025.Tests;
+
+public static class Day03Tests
+{
+    [Fact]
+    public static void Part1()
+    {
+        var solution = new Day03();
+        const string input = """
+                             987654321111111
+                             811111111111119
+                             234234234234278
+                             818181911112111
+                             """;
+        
+        var answer = solution.SolvePart1(input);
+        
+        answer.ShouldBe(357);
+    }
+    
+    [Fact]
+    public static void Part2()
+    {
+        var solution = new Day03();
+        const string input = """
+                             987654321111111
+                             811111111111119
+                             234234234234278
+                             818181911112111
+                             """;
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe(3121910778619);
+    }
+}
---
+++ 2025/D03/Day03.cs
@@ -0,0 +1,31 @@
+﻿namespace Basje.AdventOfCode.Y2025.D03;
+
+public class Day03 : Solution<IEnumerable<string>>
+{
+    protected override IEnumerable<string> ParseInput(string input)
+    {
+        return input
+            .PerLine()
+            .IgnoreEmptyLines();
+    }
+    
+    protected override object SolvePart1(IEnumerable<string> banks)
+    {
+        var sum = 0;
+
+        foreach (var bank in banks)
+        {
+            var x = bank[..^1].ToCharArray().Select(c => c - '0').Max();
+            var firstDigitLoc = bank.IndexOf(x.ToString());
+            var rest = bank[(firstDigitLoc + 1)..] + bank[^1];
+            var y = rest.ToCharArray().Select(c => c - '0').Max();
+            sum += x * 10 + y;
+        }
+        return sum;
+    }
+
+    protected override object SolvePart2(IEnumerable<string> banks)
+    {
+        return 'X';
+    }
+}
