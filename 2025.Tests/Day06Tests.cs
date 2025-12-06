289be28d24d6b9e0005a1f688eca7ab651c510b4
Bas <2102620+Basje@users.noreply.github.com>
2025-12-06 14:42:09 +0100

    Solve 2025 day 6 part 1

---
+++ 2025.Tests/Day06Tests.cs
@@ -0,0 +1,35 @@
+﻿using Basje.AdventOfCode.Y2025.D06;
+
+namespace Basje.AdventOfCode.Y2025.Tests;
+
+public static class Day06Tests
+{
+    [Fact]
+    public static void Part1()
+    {
+        var solution = new Day06();
+        const string input = """
+                             123 328  51 64 
+                              45 64  387 23 
+                               6 98  215 314
+                             *   +   *   + 
+                             """;
+        
+        var answer = solution.SolvePart1(input);
+        
+        answer.ShouldBe(4277556);
+    }
+    
+    [Fact]
+    public static void Part2()
+    {
+        var solution = new Day06();
+        const string input = """
+                             X
+                             """;
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe("X");
+    }
+}
---
+++ 2025/D06/Day06.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Basje.AdventOfCode.Y2025.D06;
+
+public class Day06 : Solution<string[][]>
+{
+    protected override string[][] ParseInput(string input)
+    {
+        return input
+            .PerLine()
+            .IgnoreEmptyLines()
+            .Select(line => Regex.Split(line, @"\s+").IgnoreEmptyLines().ToArray())
+            .ToArray();
+    }
+    
+    protected override object SolvePart1(string[][] problems)
+    {
+        var operators = problems.Last();
+        var numbers = problems
+            .Take(problems.Length - 1)
+            .Select(line => line.Select(int.Parse).ToArray())
+            .ToArray();
+        var total = new long[numbers[0].Length];
+        
+        foreach (var line in numbers)
+        {
+            foreach (var (number, i) in line.Select((number, i) => (number, i)))
+            {
+                total[i] = operators[i] switch
+                {
+                    "+" => number + total[i],
+                    "*" => total[i] == 0 ? number : number * total[i],
+                    _ => throw new Exception($"Unknown operator {operators[i]}")
+                };
+            }
+        }
+        
+        return total.Sum();
+    }
+
+    protected override object SolvePart2(string[][] problems)
+    {
+        return "X";
+    }
+}
