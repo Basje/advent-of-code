aa816d9c9071f8b760be29aed5e9d768c1df3b02
Bas <2102620+Basje@users.noreply.github.com>
2025-12-07 09:52:52 +0100

    Solve 2025 day 7 part 1

---
+++ 2025.Tests/Day07Tests.cs
@@ -0,0 +1,47 @@
+﻿using Basje.AdventOfCode.Y2025.D07;
+
+namespace Basje.AdventOfCode.Y2025.Tests;
+
+public static class Day07Tests
+{
+    [Fact]
+    public static void Part1()
+    {
+        var solution = new Day07();
+        const string input = """
+                             .......S.......
+                             ...............
+                             .......^.......
+                             ...............
+                             ......^.^......
+                             ...............
+                             .....^.^.^.....
+                             ...............
+                             ....^.^...^....
+                             ...............
+                             ...^.^...^.^...
+                             ...............
+                             ..^...^.....^..
+                             ...............
+                             .^.^.^.^.^...^.
+                             ...............  
+                             """;
+        
+        var answer = solution.SolvePart1(input);
+        
+        answer.ShouldBe(21);
+    }
+    
+    [Fact]
+    public static void Part2()
+    {
+        var solution = new Day07();
+        const string input = """
+                             X  
+                             """;
+        
+        var answer = solution.SolvePart2(input);
+        
+        answer.ShouldBe("X");
+    }
+}
---
+++ 2025/D07/Day07.cs
@@ -0,0 +1,64 @@
+﻿namespace Basje.AdventOfCode.Y2025.D07;
+
+public class Day07 : Solution<char[][]>
+{
+    protected override char[][] ParseInput(string input)
+    {
+        return input
+            .PerLine()
+            .IgnoreEmptyLines()
+            .Select(line => line.ToCharArray())
+            .ToArray();
+    }
+    
+    protected override object SolvePart1(char[][] diagram)
+    {
+        var numberOfSplits = 0;
+        // var beamLocations = new int
+        for (var y = 0; y < diagram.Length; y+=2)
+        {
+            for (var x = 0; x < diagram[y].Length; x++)
+            {
+                if (y == 0)
+                {
+                    // Search where tachyon beam enters the manifold
+                    if (diagram[y][x] == 'S')
+                    {
+                        diagram[y + 1][x] = '|';
+                        break;
+                    }
+                    continue;
+                }
+                
+                if (diagram[y][x] == '^' && diagram[y - 1][x] == '|')
+                {
+                    numberOfSplits++;
+                    diagram[y + 1][x - 1] = '|';
+                    diagram[y + 1][x + 1] = '|';
+                }
+                
+                if (diagram[y][x] == '.' && diagram[y - 1][x] == '|')
+                {
+                    diagram[y + 1][x] = '|';
+                }
+                
+            }
+        }
+
+        // for (var y = 0; y < diagram.Length; y++)
+        // {
+        //     for (var x = 0; x < diagram[y].Length; x++)
+        //     {
+        //         Console.Write(diagram[y][x]);
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        return numberOfSplits;
+    }
+
+    protected override object SolvePart2(char[][] diagram)
+    {
+        return "X";
+    }
+}
